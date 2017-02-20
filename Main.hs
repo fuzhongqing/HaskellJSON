d870da2f3408d8d153d9b7d3c104344b4f6ba049
zhongqing fu <zhongqing fu>
2017-02-20 23:46:10 +0800

    添加输出JSON功能

--- Main.hs
+++ Main.hs
@@ -1,5 +1,7 @@
 module Main (main) where
 
 import JSON
+import PutJSON
 
-main = print (JObject [("foo", JNumber 1), ("bar", JBool False)])
+-- main = print (JObject [("foo", JNumber 1), ("bar", JBool False)])
+main = putJValue (JObject [("foo", JNumber 1), ("bar", JBool False)])
